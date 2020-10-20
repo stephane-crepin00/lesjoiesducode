656015dfb282fc037ce19ca3faafd19a1fc80887
stephane <steph.crep80@gmail.com>
2020-10-20 16:47:54 +0200

    Execices

---
+++ src/Exercices.js
@@ -0,0 +1,63 @@
+function square_rectangle(largeur, longueur) {
+    return (largeur * longueur)
+}
+
+function perimètre_rectangle(largeur, longueur) {
+    return ((largeur + longueur) * 2)
+}
+
+function square_triangle_rectangle(base, hauteur) {
+    return ((base * hauteur) / 2)
+}
+
+function perimètre_triangle_rectangle(largeur, longueur) {
+    return (2 *(largeur + longueur))
+}
+
+function convert_Kelvin_to_Celsius(K) {
+    return (K - 273.15)
+}
+
+function perimètre_cercle(rayon) {
+    return (2 * Math.PI * rayon)
+}
+
+function square_cercle(rayon) {
+    return ((rayon * rayon) * Math.PI)
+}
+
+function give_me_money(price, billAmount) {
+    return (billAmount - price)
+}
+
+function how_many_days(prix, budget) {
+    const a = budget % prix
+    const b = budget - a
+    const c = b / prix
+    return `Je peux partir ${c} jours et il me reste ${a} euros`
+}
+
+function pair(nombre) {
+    return ((nombre % 2) === 0)
+}
+
+function delete_first_letter(word) {
+    return word.substr(1)
+}
+
+function delete_last_letter(word) {
+    return word.slice(0, -1)
+}
+
+console.log(square_rectangle(10, 14))
+console.log(perimètre_rectangle(10, 14))
+console.log(square_triangle_rectangle(20, 18))
+console.log(perimètre_triangle_rectangle(20, 18))
+console.log(convert_Kelvin_to_Celsius(12))
+console.log(perimètre_cercle(8))
+console.log(square_cercle(8))
+console.log(give_me_money(15, 20))
+console.log(how_many_days(10, 100))
+console.log(pair(28))
+console.log(delete_first_letter("bonjour"))
+console.log(delete_last_letter("Bonjour"))
---
+++ src/variable.js
@@ -0,0 +1,14 @@
+let a = 2;
+const d = "toto"
+const e = " etui"
+const b = 4;
+let vrai = true;
+let faux = false;
+const f = d + e
+let c = b + b;
+c = b - a;
+a++
+
+a = 5;
+
+console.log(f);
